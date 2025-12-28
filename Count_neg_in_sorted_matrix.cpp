f4b545c50ed7a105b800d4ea20f6e0815c0f062c
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2025-12-28 21:55:55 +0530

    Update Count_neg_in_sorted_matrix.cpp -- added O(m*logn)

--- Count_neg_in_sorted_matrix.cpp
+++ Count_neg_in_sorted_matrix.cpp
@@ -19,3 +19,24 @@ public:
 
 // tc --> O(n*m)
 // sc --> O(1)
+
+// --------- optimal solution---------
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int rowsize = grid.size();
+        int columnsize = grid[0].size();
+        int count=0;
+
+        for(auto &row: grid)
+        {
+            int idx = upper_bound(begin(row),end(row),0,greater<int>()) - begin(row);
+            count+=(columnsize-idx);
+        }
+        return count;
+    }
+};
+
+// tc --> O(m * logn)
+// sc --> O(1)
