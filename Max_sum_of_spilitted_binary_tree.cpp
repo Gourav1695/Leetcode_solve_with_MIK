8f41c643ce44e5135c349cf09e8a6aec551efad6
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-01-07 23:38:23 +0530

    Update Max_sum_of_spilitted_binary_tree.cpp

--- Max_sum_of_spilitted_binary_tree.cpp
+++ Max_sum_of_spilitted_binary_tree.cpp
@@ -58,3 +58,56 @@ public:
 
 // tc -->O(n)
 // sc -->O(H)
+
+
+
+
+// ------------------- 2nd way with only one totalSum function -----------------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long  maxP    = 0;
+    int SUM     = INT_MIN;
+    int totalSum(TreeNode* root)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        int leftSum = totalSum(root->left);
+        int rightSum = totalSum(root->right);
+
+        long long  subTreeSum = root->val + leftSum + rightSum;
+        long long  remainingTreeSum = SUM - subTreeSum;
+        
+        maxP = max(maxP, (subTreeSum*remainingTreeSum));
+        return subTreeSum;
+    }
+
+    int maxProduct(TreeNode* root) {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        maxP = 0;
+        SUM = totalSum(root);
+        cout<<"SUM="<<SUM<<endl;
+        totalSum(root);
+        cout<<maxP<<endl;
+        return maxP%1000000007;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(H)
