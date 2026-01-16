62587b383f9d15dc41a442b1d08345a2a5a7434f
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-01-16 23:48:37 +0530

    Create Max_square_area_from_removing_fences_from_a_field.cpp

---
+++ Max_square_area_from_removing_fences_from_a_field.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+        int  M = 1e9+7;
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        hFences.push_back(1);
+        hFences.push_back(m);
+
+        vFences.push_back(1);
+        vFences.push_back(n);
+
+        sort(hFences.begin(),hFences.end());
+        sort(vFences.begin(),vFences.end());
+        unordered_set<int>heights;
+        unordered_set<int>widths;
+
+
+        for(int i = 0; i<hFences.size()-1;i++){
+            for(int j = i+1; j<hFences.size();j++ ){
+                int height = hFences[j]-hFences[i];
+                heights.insert(height);
+            }
+        }
+        int maxSide = 0;
+        for(int i = 0; i<vFences.size()-1;i++){
+            for(int j = i+1; j<vFences.size();j++ ){
+                int width = vFences[j]-vFences[i];
+                if(heights.find(width)!=heights.end())
+                {
+                    maxSide = max(maxSide,width);
+                }
+            }
+        }
+        return (maxSide == 0) ? -1 : (1LL * maxSide * maxSide)%M;
+        
+    }
+};
+
+// tc --> O(HlogH + VlogV + H^2+V^2) ~ O(H^2+V^2);
+// sc --> O(H^2)
