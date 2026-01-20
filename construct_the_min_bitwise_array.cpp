9c8ef235bdd3a10ed3764130a582173e99322d0b
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-01-21 01:05:34 +0530

    Update construct_the_min_bitwise_array.cpp

--- construct_the_min_bitwise_array.cpp
+++ construct_the_min_bitwise_array.cpp
@@ -23,3 +23,40 @@ public:
 
 // tc --> O(n*m)
 // sc --> O(1)
+
+// optimal appraoch
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>result;
+       for(int i = 0 ;i<n;i++)
+       {
+            if (nums[i]==2){
+                result.push_back(-1);
+                continue;
+            }
+            bool found = false;
+            for(int j = 1; j<32;j++)
+            {
+                if((nums[i]&(1<<j))>0){ // checking set bit
+                    continue;
+                }
+                int prev = (j-1);
+                // Making prev bit 0
+                int x = (nums[i]^(1<<(j-1)));
+                result.push_back(x);
+                found = true;
+                break;
+            }
+            if(found == false){
+                result.push_back(-1);
+            }
+       }
+       return result;
+    }
+};
+
+// tc --> O(n)
+// sc --> O(1)
