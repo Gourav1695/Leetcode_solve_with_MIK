922fb2532f02b3471be4e1ba3ff1d17a2191959e
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-01-24 12:55:57 +0530

    Create Min_max_pair_sum_in_array.cpp

---
+++ Min_max_pair_sum_in_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+     int n = nums.size();
+     int maxSum = 0;
+     for (int i = 0; i < n / 2; i++)
+     {
+        maxSum = max(maxSum, nums[i] + nums[n-i-1]);
+     }
+     return maxSum;
+    }
+};
+
+// tc : O(nlogn) for sorting
+// sc : O(1)
