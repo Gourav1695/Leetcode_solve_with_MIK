b5247508d83734c3a5b0ee84672c9be1cd86bb99
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-01-31 13:35:03 +0530

    Update Find_smallest_letter_greater_than_target.cpp

--- Find_smallest_letter_greater_than_target.cpp
+++ Find_smallest_letter_greater_than_target.cpp
@@ -15,3 +15,34 @@ public:
 };
 // tc -->O(n)
 // sc -->O(1)
+
+// -------------------- Binary Search approach: -----------------
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        // binary search Implementation
+        int l = 0;
+        int r = letters.size()-1;
+        
+        int position = -1;
+        while(l<=r){
+            int mid = l+ (r-l)/2;
+            if(letters[mid]>target){
+                r = mid-1;
+                position= mid;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        cout<<position<<endl;
+        if(position==-1){
+            return letters[0];
+        }
+        return letters[position];
+
+    }
+};
+
+// tc -->O(logn)
+// sc -->O(1)
