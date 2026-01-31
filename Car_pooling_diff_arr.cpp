d718a8bc473c8a9cf65671ef6f7a6c1e2d94a771
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-01-31 11:47:40 +0530

    Update Car_pooling_diff_arr.cpp

--- Car_pooling_diff_arr.cpp
+++ Car_pooling_diff_arr.cpp
@@ -24,3 +24,29 @@ public:
 
 // tc -->O(n)
 // sc -->O(1)
+
+
+// ----------------------- Line Sweep Algorithm solution:-----------------
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        map<int,int>mp;
+        for(auto &it: trips){
+            mp[it[1]]+=it[0];
+            mp[it[2]]-=it[0];
+        }
+        int sum =0;
+        for(auto & it: mp)
+        {
+            sum +=it.second;
+            if(sum>capacity)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// tc --> O(nlogn)
+// sc --> O(n)
