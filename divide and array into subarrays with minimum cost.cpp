a786f1e910f50a6edacb2c4844139115b4dd10c2
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-01 13:09:23 +0530

    Update divide and array into subarrays with minimum cost.cpp

--- divide and array into subarrays with minimum cost.cpp
+++ divide and array into subarrays with minimum cost.cpp
@@ -13,3 +13,32 @@ public:
 };
 // tc -->O(nlogn)
 // sc -->O(1)
+
+
+// ----------------- using firstMin and secondMin :----------------
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        // second approach using simple firstmin and secondmin
+
+        int n = nums.size();
+        int score = nums[0];
+        int firstMin = INT_MAX;
+        int secondMin= INT_MAX;
+
+        for(int i =1; i<n;i++)
+        {
+            if(firstMin>nums[i]){
+                secondMin = firstMin;
+                firstMin  = nums[i];
+            }
+            else if( secondMin>nums[i]){
+                secondMin = nums[i];
+            }
+        }
+        return score + firstMin + secondMin;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
