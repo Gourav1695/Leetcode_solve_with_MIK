dad56f6f73542ece18f6d8230195e3be3b427383
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-03 11:16:33 +0530

    Update Trionic_array_1.cpp

--- Trionic_array_1.cpp
+++ Trionic_array_1.cpp
@@ -34,3 +34,31 @@ public:
 
 // tc -->O(n)
 // sc -->O(1)
+
+
+// ----------- Approach good way : ----------
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        while(i+1 < n && nums[i+1]>nums[i]){
+            i++;
+        }
+        if(i==0 || i == n-1) return false;
+        while(i+1 < n && nums[i+1]<nums[i]){
+            i++;
+        }
+        if(i == n-1) return false;
+        while(i+1 < n && nums[i+1]>nums[i]){
+            i++;
+        }
+        if(i == n-1){
+            return true;
+        }
+        return false;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
