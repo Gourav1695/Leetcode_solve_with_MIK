5f4a71b36dbab55921adca82168e50f87f999430
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-05 17:56:59 +0530

    Update transformmered_array.cpp

--- transformmered_array.cpp
+++ transformmered_array.cpp
@@ -14,3 +14,28 @@ public:
 };
 // tc --> O(n)
 // sc --> O(n)
+
+
+// ------------ Approach 2:--------------
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>vec;
+
+        for(int i = 0 ; i<n;i++)
+        {
+            int shifts= nums[i]%n; // normalizing the size
+
+            int newIdx = (i+shifts)%n; // checking of shifts
+            if(newIdx<0){ // if the shift is negative out of bound then adding n to it
+                newIdx +=n;
+            }
+            vec.push_back(nums[newIdx]);
+        }
+        return vec;
+    }
+};
+// tc --> O(n)
+// sc --> O(n)
