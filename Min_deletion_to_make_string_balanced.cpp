58716f11d5de699ee74b29dff2bf2cf7c2b6dfe8
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-07 13:11:47 +0530

    Update Min_deletion_to_make_string_balanced.cpp

--- Min_deletion_to_make_string_balanced.cpp
+++ Min_deletion_to_make_string_balanced.cpp
@@ -63,3 +63,36 @@ public:
 
 // tc -->O(2n)~ O(n)
 // sc --> O(n)
+
+
+// ----------------- Approach 3 : most optimal: ----------
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.length();
+        int count_a = 0;
+
+        for(int i = s.length()-1; i>=0; i--)
+        {
+            if(s[i]=='a'){
+                count_a++;
+            }
+        }
+        int countb = 0;
+        int minDeletion = INT_MAX;
+        for(int i= 0 ; i<s.length(); i++)
+        {
+            if(s[i]=='a'){
+                count_a--;
+            }
+            minDeletion = min(minDeletion,(count_a+countb));
+            if(s[i]=='b'){
+                countb++;
+            }
+        }
+        return minDeletion;
+    }
+};
+
+// tc --> O(1)
+// sc --> O(n)
