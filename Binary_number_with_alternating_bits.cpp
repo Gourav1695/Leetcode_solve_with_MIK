4ccd7133510314882ea26738b61f79613d09c7aa
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-18 22:28:36 +0530

    Update Binary_number_with_alternating_bits.cpp

--- Binary_number_with_alternating_bits.cpp
+++ Binary_number_with_alternating_bits.cpp
@@ -15,3 +15,20 @@ public:
 
 // tc -->O(1)
 // sc -->O(1)
+
+// ----------------- Appraoch 2: ------------------
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int highestSetBit = log2(n);
+        bitset<32>b(n);
+        for(int i = 0 ; i<=highestSetBit; i++){
+            if(b[i]== b[i+1])
+            return false;
+        }
+        return true;
+    }
+};
+// tc -->O(logn)
+// sc --> O(1)
