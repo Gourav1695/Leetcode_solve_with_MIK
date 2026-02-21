efe4c0e7f9e4452feb8ae60e4c4bc6baea333ca6
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-21 22:13:53 +0530

    Update Prime_no_of_set_bits_in_binary.cpp

--- Prime_no_of_set_bits_in_binary.cpp
+++ Prime_no_of_set_bits_in_binary.cpp
@@ -27,3 +27,35 @@ public:
 
 // tc -->O(n*sqrt(m))
 // sc -->O(1)
+// --------------- Appraoch 2:---------------
+
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i = 2; i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    int countPrimeSetBits(int left, int right) {
+        set<int>st={2,3,5,7,11,13,17,19};
+        int count = 0;
+        int x = 0;
+        for(int i = left; i<=right; i++){
+            x = __builtin_popcount(i);
+            cout<<x<<" ";
+            if(st.count(x)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+// tc -->O(k), here k is right-left+1
+// sc -->O(1)
