d6b399f8c3484091f244fd9396ba55d905f9b28b
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-27 16:21:14 +0530

    Update Leetcode_1404_26feb_potd.cpp

--- Leetcode_1404_26feb_potd.cpp
+++ Leetcode_1404_26feb_potd.cpp
@@ -33,3 +33,27 @@ public:
 
 // tc -->O(n^2)
 // sc-->O(1)
+
+//------------- Optimal Appraoch : ----------
+
+class Solution {
+public:
+    int numSteps(string s) {
+        int n = s.length();
+        int op = 0;
+        int carry = 0; 
+        for( int i = n-1; i>=1; i--){
+            if((s[i]-'0' + carry)%2 == 1){
+                op+=2;
+                carry = 1;
+            }
+            else{
+                op++;
+            }
+        }
+        return op + carry;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
