18032b1529b076605de16507904c89cc9f3b4445
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-02-28 13:10:26 +0530

    Update concat_of_consecutive_binary_number.cpp

--- concat_of_consecutive_binary_number.cpp
+++ concat_of_consecutive_binary_number.cpp
@@ -15,3 +15,23 @@ public:
 
 // tc -->O(n)
 // sc -->O(1)
+
+// ---------- Appraoch 2 : ------------
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        int m = 1e9+7;
+        int digits=0;
+        long result=0;
+        for(int i = 1; i<=n;i++) // O(n)
+        {
+            if((i & (i-1))==0)
+                digits++; // O(1)
+            result = ((result<<digits)%m + i)%m; //O(1)
+        }
+        return result;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
