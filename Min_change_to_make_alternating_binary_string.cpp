20267fde86b1c08defaa81d9f2daf9494be7de2c
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-05 11:48:33 +0530

    Create Min_change_to_make_alternating_binary_string.cpp

---
+++ Min_change_to_make_alternating_binary_string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int start0 = 0;
+        int start1 = 0;
+
+        for(int i = 0; i<s.length();i++)
+        {
+            if(i%2 == 0){
+                if(s[i]=='0'){
+                    start1++;
+                }
+                else{
+                    start0++;
+                }
+            }
+            else{
+                if(s[i]=='1'){
+                    start1++;
+                }
+                else{
+                    start0++;
+                }
+            }
+        }
+        return min(start0,start1);
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
