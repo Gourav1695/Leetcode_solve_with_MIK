124a0bfd7fce1048007728b250be4dcb27e1fbfd
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-05 15:26:37 +0530

    Update Min_change_to_make_alternating_binary_string.cpp

--- Min_change_to_make_alternating_binary_string.cpp
+++ Min_change_to_make_alternating_binary_string.cpp
@@ -29,3 +29,33 @@ public:
 
 // tc -->O(n)
 // sc -->O(1)
+
+
+// ------- Another Approach: ------------
+class Solution {
+public:
+    int minOperations(string s) {
+        int start0 = 0;
+        int start1 = 0;
+
+        for(int i = 0; i<s.length();i++)
+        {
+            if(i%2 == 0){
+                if(s[i]=='0'){
+                    start1++;
+                }
+            }
+            else{
+                if(s[i]=='1'){
+                    start1++;
+                }
+
+            }
+        }
+        start0 = s.length() - start1;
+        return min(start0,start1);
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
