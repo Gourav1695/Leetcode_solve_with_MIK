e300554d59d2bc81f067d87713b3c8d23792bbc8
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-06 11:11:48 +0530

    Update Check_if_binary_string_has_atmost_one_segment_of_zeros.cpp

--- Check_if_binary_string_has_atmost_one_segment_of_zeros.cpp
+++ Check_if_binary_string_has_atmost_one_segment_of_zeros.cpp
@@ -12,3 +12,32 @@ public:
 
 // tc -->O(n)
 // sc -->O(1)
+
+
+//----------- Appraoch 2 :---------
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        int n = s.length();
+        int count = 0;
+        int i = 0;
+        while(i<n){
+            if(s[i]=='1'){
+                count++;
+                while(i<n && s[i]=='1'){
+                    i++;
+                }
+            }
+            else{
+                i++;
+            }
+            if(count>1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
