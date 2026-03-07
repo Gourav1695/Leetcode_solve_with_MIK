84927f073d9ffc969679ce26459df3eb8245fb8b
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-08 01:47:00 +0530

    Update Min_no_of_flips_to_make_the_string_alternating.cpp

--- Min_no_of_flips_to_make_the_string_alternating.cpp
+++ Min_no_of_flips_to_make_the_string_alternating.cpp
@@ -50,7 +50,7 @@ public:
 // tc -->O(n)
 // sc -->O(2*n)
 
-//--------------- Approach 2 : with O(1) space:------------
+//--------------- Approach 2 : with O(n) space:------------
 
 class Solution {
 public:
@@ -86,6 +86,54 @@ public:
                     flip1--;
                 }
                 if(s[i%n]!=s2[i]){
+                    flip2--;
+                }
+                i++;
+
+            }
+            if(j-i+1 == n){
+                result = min({result, flip1, flip2});
+            }
+            j++;
+        }
+        return result;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(n)
+
+// ------------ Most optimal approach space O(1)------------
+
+class Solution {
+public:
+    int minFlips(string s) {
+        int n = s.length();
+
+        // from here sliding window starts
+        int result = INT_MAX;
+        int i = 0;
+        int j = 0;
+        int flip1 = 0;
+        int flip2 = 0;
+        while(j<2*n){
+            char expecteds1 = (j%2==0? '0': '1');
+            char expecteds2 = (j%2==0? '1': '0');
+            if(s[j%n]!=expecteds1){
+              flip1++;  
+            }
+            if(s[j%n]!=expecteds2){
+              flip2++;  
+            }
+
+            if(j-i+1>n){ // need of shrinking of the window
+
+            expecteds1 = (i%2==0? '0': '1');
+            expecteds2 = (i%2==0? '1': '0');
+                if(s[i%n]!=expecteds1){
+                    flip1--;
+                }
+                if(s[i%n]!=expecteds2){
                     flip2--;
                 }
                 i++;
