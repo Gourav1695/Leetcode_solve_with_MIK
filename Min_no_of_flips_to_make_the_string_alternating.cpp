751e215a0b3384d1e32ca28cf31519de27bb39f4
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-08 01:34:48 +0530

    Update Min_no_of_flips_to_make_the_string_alternating.cpp

--- Min_no_of_flips_to_make_the_string_alternating.cpp
+++ Min_no_of_flips_to_make_the_string_alternating.cpp
@@ -49,3 +49,56 @@ public:
 
 // tc -->O(n)
 // sc -->O(2*n)
+
+//--------------- Approach 2 : with O(1) space:------------
+
+class Solution {
+public:
+    int minFlips(string s) {
+        int n = s.length();
+
+        // create the alternating string 
+        // s1 and s2
+        string s1="";
+        string s2="";
+        for(int i = 0; i<2*n ; i++)
+        {
+            s1+=(i%2==0? '0': '1');
+            s2+=(i%2==0? '1': '0');
+        }
+
+        // from here sliding window starts
+        int result = INT_MAX;
+        int i = 0;
+        int j = 0;
+        int flip1 = 0;
+        int flip2 = 0;
+        while(j<2*n){
+            if(s[j%n]!=s1[j]){
+              flip1++;  
+            }
+            if(s[j%n]!=s2[j]){
+              flip2++;  
+            }
+
+            if(j-i+1>n){ // need of shrinking of the window
+                if(s[i%n]!=s1[i]){
+                    flip1--;
+                }
+                if(s[i%n]!=s2[i]){
+                    flip2--;
+                }
+                i++;
+
+            }
+            if(j-i+1 == n){
+                result = min({result, flip1, flip2});
+            }
+            j++;
+        }
+        return result;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
