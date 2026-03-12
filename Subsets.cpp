0266ce8e47e140dd0e87c2dc80e3cb682c899d25
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-13 01:56:14 +0530

    Create Subsets.cpp

---
+++ Subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    void solve(vector<int>& nums, vector<int>&curr,int idx){
+        if(idx>=nums.size()){
+            result.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[idx]);
+        solve(nums, curr, idx+1);
+        curr.pop_back();
+        solve(nums, curr, idx+1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+      vector<int>curr;
+      solve(nums,curr, 0);
+      sort(result.begin(),result.end());
+      return result; 
+    }
+};
+
+
+// tc -->O(2^n)
+// sc -->O(n)
