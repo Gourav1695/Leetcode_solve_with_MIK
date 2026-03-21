96fdaebb840e152deb88eea00ba69d17d22a1c93
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-21 16:52:09 +0530

    Update Flip_square_submatrix_vertically.cpp

--- Flip_square_submatrix_vertically.cpp
+++ Flip_square_submatrix_vertically.cpp
@@ -18,3 +18,27 @@ public:
 
 // tc --> O(k^2)
 // sc --> O(1)
+
+//------------ MIK's style:-------------
+
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        int startrow = x;
+        int startcol = y;
+        int endrow = x+k-1;
+        int endcol = y+k-1;
+
+        for(int i = startrow; i<=endrow; i++)
+        {
+            for(int j = startcol; j <= endcol;j++){
+                swap(grid[i][j],grid[endrow][j]);
+            }
+            endrow--;
+        }
+        return grid;
+    }
+};
+
+// tc --> O(k^2)
+// sc --> O(1)
