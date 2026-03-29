d1b55275398a45cdaea5bd6e42197d6dfc9a1775
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-03-29 17:11:42 +0530

    Update Check_if_string_can_be_made_equal_using_operation1.cpp

--- Check_if_string_can_be_made_equal_using_operation1.cpp
+++ Check_if_string_can_be_made_equal_using_operation1.cpp
@@ -11,3 +11,34 @@ public:
 
 // tc -->O(1)
 // sc-->O(1)
+
+
+// --------------------- Another apporach 2:-----------------
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        vector<int>even(26,0); // vector<int> v(size, initial_value);
+        vector<int>odd(26,0);
+
+        for(int i = 0 ; i<4; i++)
+        {
+            if(i%2==0){
+                even[s1[i]-'a']++;
+                even[s2[i]-'a']--;
+            }
+            else{
+                odd[s1[i]-'a']++;
+                odd[s2[i]-'a']--;
+            }
+        }
+        for(int i = 0; i<26; i++){
+            if(even[i]!=0 ||odd[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// tc -->O(1)
+// sc -->O(1)
