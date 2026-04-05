f8f0577aefa7a572ea03e17cb8a220dcc9f6c44b
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-04-05 12:20:56 +0530

    Update Robot_return_to_origin.cpp

--- Robot_return_to_origin.cpp
+++ Robot_return_to_origin.cpp
@@ -24,3 +24,37 @@ public:
 
 // tc -->O(n)
 // sc -->O(1)
+
+
+// ------------ Appraoch 2 : using switch statement:------------
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for(int i = 0 ; i<moves.size();i++)
+        {
+            switch(moves[i]){
+                case 'U':
+                y++;
+                break;
+                case 'R':
+                x++;
+                break;
+                case 'L':
+                x--;
+                break;
+                case 'D':
+                y--;
+            }
+        }
+        if(x==0 && y == 0 )
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
