313fb6624c8a699f6e251cb7b4a5e991724ffc7a
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-04-05 23:58:55 +0530

    Update Find_the_winner_of_the_circular_game.cpp

--- Find_the_winner_of_the_circular_game.cpp
+++ Find_the_winner_of_the_circular_game.cpp
@@ -43,3 +43,24 @@ public:
 
 // tc --> O(n*k)
 // sc --> O(n)
+
+// ----------------- Appraoch 3 : recurrsion (most optimal)------------
+class Solution {
+public:
+    int findWinnerIdx(int n , int k){
+        // base case
+        if( n==1 ){
+            return 0; // index of only one element left is 0
+        }
+        int idx = findWinnerIdx(n-1,k);
+        idx = (idx+k)%n; // to find the original index in the original array
+        return idx;
+    }
+    int findTheWinner(int n, int k) {
+        int result_idx = findWinnerIdx(n,k);
+        return result_idx+1;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
