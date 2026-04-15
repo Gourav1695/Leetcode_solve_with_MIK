e45bdb7b05390c2fab97e88fc6569e2711b0cbbf
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-04-16 00:44:07 +0530

    Update Shortest_distance_to_target_string_in_circular_array.cpp

--- Shortest_distance_to_target_string_in_circular_array.cpp
+++ Shortest_distance_to_target_string_in_circular_array.cpp
@@ -13,5 +13,28 @@ public:
         return ans==INT_MAX? -1: ans;
     }
 };
-// tc -->O(n)
+// tc -->O(n*l)
+// sc -->O(1)
+
+
+// ----------- optimal appraoch MIK:--------
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        int ans = INT_MAX;
+        int n = words.size();
+
+        for(int i = 0; i<n;i++){
+            if(words[i] == target){
+            int stDist = abs(i - startIndex);
+            int cirDist = abs(n - stDist);
+            ans = min({ans, stDist, cirDist});
+            }
+        }
+        return ans == INT_MAX? -1: ans;
+
+    }
+};
+
+// tc -->O(n * l), here n is length of words vector, and l is the size of target for comparison
 // sc -->O(1)
