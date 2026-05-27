ec1aac14496ba3fd6ab62ebfd16432714484d30e
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-05-27 15:31:50 +0530

    Update Count_the_no_of_spacial_char_1.cpp

--- Count_the_no_of_spacial_char_1.cpp
+++ Count_the_no_of_spacial_char_1.cpp
@@ -18,3 +18,25 @@ public:
 
 // tc -->O(n)
 // sc -->O(1)
+
+// ---------- second Appraoch-----------------
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        int freq[123] = {0}; // freq arr
+        int count = 0;
+        for(auto it: word){
+            freq[it]++; // a to z 97 to 122 , A to Z 65 to 90
+        }
+        for(int i = 'a' ; i<='z'; i++){
+            int capIdx =  i-'a'+'A';
+            if(freq[i]>0 && freq[capIdx]>0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+// tc -->O(n)
+// sc -->O(1)
