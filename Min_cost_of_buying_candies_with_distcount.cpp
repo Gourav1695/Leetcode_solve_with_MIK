b1aacdf4743ee92b8f6a327f717703a0ab3d5991
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-06-01 11:05:28 +0530

    Update Min_cost_of_buying_candies_with_distcount.cpp

--- Min_cost_of_buying_candies_with_distcount.cpp
+++ Min_cost_of_buying_candies_with_distcount.cpp
@@ -19,3 +19,21 @@ public:
 
 // tc-->O(nlogn)
 // sc -->O(1)
+
+
+// -------------- optimal approach : -------------
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        int ans = 0;
+        sort(cost.rbegin(),cost.rend());
+        for(int i = 0; i<cost.size();i++){
+            if((i+1)%3==0) continue;
+            ans+=cost[i];
+        }
+        return ans;
+    }
+};
+
+// tc --> O(nlogn)
+// sc --> O(1)
