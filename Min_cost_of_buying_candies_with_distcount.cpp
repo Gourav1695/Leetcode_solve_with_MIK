22cd914de4364212a6374a1020f8a87fbe17279e
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2026-06-01 10:45:31 +0530

    Create Min_cost_of_buying_candies_with_distcount.cpp

---
+++ Min_cost_of_buying_candies_with_distcount.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        int total = 0;
+       for(int i = 0; i<cost.size(); i++){
+        total+=cost[i];
+       } 
+       if(cost.size()==1||cost.size()==2){
+        return total;
+       }
+       sort(cost.rbegin(),cost.rend());
+       int rem = 0;
+       for(int i = 2;i<cost.size();i+=3){
+            rem +=cost[i];
+       }
+       return total-rem;
+    }
+};
+
+// tc-->O(nlogn)
+// sc -->O(1)
